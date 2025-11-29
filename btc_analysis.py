24222cd8e3df1788608db79cd8ad0f06af159d6a
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-11-29 04:30:37 +0100

    AI Update to btc_analysis.py

--- btc_analysis.py
+++ btc_analysis.py
@@ -115,7 +115,7 @@ def calculate_capital(df, initial_capital=1000):
         if signal == 'long' and position != 'long':
             if position == 'short':
                 # Close short position
-                capital *= (prev_row['close'] / buy_price_short)
+                capital *= (2 - (prev_row['close'] / buy_price_short))
             # Open long position
             position = 'long'
             buy_price_long = row['close']
