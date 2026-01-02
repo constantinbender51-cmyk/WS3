9a2e742016b3f78f9cd536ad0aa61526e40ce156
Constantin Bender <constantinbender51@gmail.com>
2026-01-02 15:51:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ PRICES = [
 WINDOW_SIZE = 12 
 
 # Penalty for switching from one action to another
-SWITCHING_PENALTY_WEIGHT = 10
+SWITCHING_PENALTY_WEIGHT = 100
 
 # Actions available at each step
 ACTIONS = ['Long', 'Hold', 'Short']
